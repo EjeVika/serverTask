4ac5bb79f7b8d71e78b30216c54a36c81ad8b514
EjeVika <khudyakovav@gmail.com>
2013-07-17 16:47:10 +0700

    Ver 4

--- src/com/sukhorukov/khudyakova/servertask/ServerProcess.java
+++ src/com/sukhorukov/khudyakova/servertask/ServerProcess.java
@@ -13,14 +13,12 @@ import java.net.Socket;
 public class ServerProcess implements Runnable{
     private Socket socket;
     private File directory;
-    private File currentDir;
     private String outFileName;
 
 
     public ServerProcess(Socket socket,File directory, String outFileName) throws IOException {
         this.socket = socket;
         this.directory = directory;
-        this.currentDir = directory;
         this.outFileName = outFileName;
     }
 
@@ -46,8 +44,8 @@ public class ServerProcess implements Runnable{
 
             switch (cmd){
                 case "GET":get(wantedPath,out);break;
-                case "HEAD":head();break;
-                default: notImpl501();
+                case "HEAD":head(wantedPath,out);break;
+                default: notImpl501(out);
             }
 
 
@@ -60,8 +58,13 @@ public class ServerProcess implements Runnable{
 
     }
 
-    private void notImpl501() {
+    private void notImpl501(OutputStream out) throws IOException {
 
+        out.write("HTTP/0.9 501 Not Implemented\r\n".getBytes());
+        out.write("Content-Type: text; charset=UTF-8\r\n".getBytes());
+    //    out.write(("Content-Length: " + wantedFile.length() + "\r\n").getBytes());
+        out.write("\r\n".getBytes());
+        out.flush();
     }
 
     private void get(String wantedPath,OutputStream out) throws IOException {
@@ -77,54 +80,40 @@ public class ServerProcess implements Runnable{
         }else{
             wantedFile = new File(directory.getCanonicalPath()+File.separator+temp);
         }
-     /*   String fileName = temp.getName();
-        File wantedFile = new File(directory.getCanonicalPath()+File.separator+temp);
-        if (fileName.equals(outFileName)){
-            DirectoryContentWriter.createDirHTML(temp,outFileName);
-            wantedFile = new File(directory.getCanonicalPath()+File.separator+temp);
-        }  */
-/*        System.out.println("temp = "+temp);
-        System.out.println("temp.getCanonicalPath() = "+temp.getCanonicalPath());
-        System.out.println("temp.getName() = "+temp.getName());
-        System.out.println("currentDir="+currentDir);
-        System.out.println("currentDir.getCanonicalPath() = "+currentDir.getCanonicalPath());
-        System.out.println("currentDir.getAbsolutePath() = "+currentDir.getAbsolutePath());
-        File wantedFile = new File(currentDir.getCanonicalPath()+File.separator+temp);
-    //    File wantedFile = new File(".."+File.separator+temp);
-        System.out.println("wantedPath = "+wantedPath);
-        System.out.println("wantedFile = "+wantedFile);
-        System.out.println("wantedFile.getCanonicalPath() = "+wantedFile.getCanonicalPath());
-        if (wantedFile.isDirectory()){
-            this.currentDir = wantedFile;
-            System.out.println("currentDir = "+currentDir);
-            DirectoryContentWriter.createDirHTML(wantedFile,outFileName);
-            wantedFile = new File(wantedFile.getAbsolutePath()+File.separator+outFileName);
-            System.out.println(wantedFile.getCanonicalPath());
-        }
-        //System.out.println(wantedPath);
-        //    DirectoryContentWriter.createDirHTML(directory,outFileName);
-        //    File indexFile = new File(directory.getAbsolutePath()+File.separator+outFileName);
-*/
-        out.write("HTTP/1.0 200 OK\r\n".getBytes());
 
+        out.write("HTTP/0.9 200 OK\r\n".getBytes());
         out.write("Content-Type: text; charset=UTF-8\r\n".getBytes());
         out.write(("Content-Length: " + wantedFile.length() + "\r\n").getBytes());
-
         out.write("\r\n".getBytes());
-
-
         out.flush();
-        try (InputStream inIndex = new FileInputStream(wantedFile)){
 
+        try (InputStream inIndex = new FileInputStream(wantedFile)){
             byte buf[] = new byte[4096];
             int count;
             while((count = inIndex.read(buf))>=0){
                 out.write(buf, 0, count);
             }
         }
+    //    out.flush();
     }
-    private void head(){
 
+    private void head(String wantedPath, OutputStream out) throws IOException {
+        File temp = new File(wantedPath);
+        File wantedFile = null;
+        if (temp.isDirectory()){
+            DirectoryContentWriter.createDirHTML(new File(directory.getCanonicalPath()+File.separator+temp),outFileName);
+            wantedFile = new File(directory.getCanonicalPath()+File.separator+outFileName);
+        }else if (temp.getName().equals(outFileName)){
+            DirectoryContentWriter.createDirHTML(new File(directory.getCanonicalPath()+File.separator+temp.getParentFile()),outFileName);
+            wantedFile = new File(directory.getCanonicalPath()+File.separator+temp);
+        }else{
+            wantedFile = new File(directory.getCanonicalPath()+File.separator+temp);
+        }
+        out.write("HTTP/0.9 200 OK\r\n".getBytes());
+        out.write("Content-Type: text; charset=UTF-8\r\n".getBytes());
+        out.write(("Content-Length: " + wantedFile.length() + "\r\n").getBytes());
+        out.write("\r\n".getBytes());
+        out.flush();
     }
 
 }
